ba7d189834259382d7d43d0f96e5c35fb08280de
Nur Bar <LCD344@yahoo.com>
2019-07-07 13:42:14 +0300

    Added service worker registration

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,5 @@
+import './serviceWorker/registerServiceWorker';
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
