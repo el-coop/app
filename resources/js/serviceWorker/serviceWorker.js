881f1bc855d3e73fb8236317725bd71dfdbe8fc8
Nur Bar <LCD344@yahoo.com>
2019-11-21 14:10:54 -0600

    added refresh when prompted

--- resources/js/serviceWorker/serviceWorker.js
+++ resources/js/serviceWorker/serviceWorker.js
@@ -2,3 +2,9 @@ workbox.core.setCacheNameDetails({prefix: "app.elcoop.io"});
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+self.addEventListener('message', (message) => {
+    if (message.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
