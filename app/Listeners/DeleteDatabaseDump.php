a5d167bc089bea26d6e8e04e83651166c736a01f
Nur Bar <LCD344@yahoo.com>
2020-04-11 18:08:37 -0500

    made listener go through queue

--- app/Listeners/DeleteDatabaseDump.php
+++ app/Listeners/DeleteDatabaseDump.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Storage;
 
-class DeleteDatabaseDump {
+class DeleteDatabaseDump implements ShouldQueue {
     /**
      * Create the event listener.
      *
