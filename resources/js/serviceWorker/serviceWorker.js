67cd64215fa82ae31082377a7433f1f1052594e6
Nur Bar <LCD344@yahoo.com>
2020-10-04 19:56:39 +0200

    fixing service worker

--- resources/js/serviceWorker/serviceWorker.js
+++ resources/js/serviceWorker/serviceWorker.js
@@ -1,16 +1,11 @@
 import { precacheAndRoute } from 'workbox-precaching';
+import {setCacheNameDetails} from 'workbox-core';
 
+setCacheNameDetails({prefix: "app.elcoop.io"});
 precacheAndRoute(self.__WB_MANIFEST);
-
-workbox.core.setCacheNameDetails({prefix: "app.elcoop.io"});
-
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener('message', (message) => {
     if (message.data.action === 'skipWaiting') {
         self.skipWaiting();
     }
 });
-
-
