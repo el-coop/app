1e6ac391f1150f9f1b34d29220472489a880b000
Nur Bar <LCD344@yahoo.com>
2023-01-25 17:12:18 -0300

    clean updated cache

--- resources/js/serviceWorker/serviceWorker.js
+++ resources/js/serviceWorker/serviceWorker.js
@@ -1,11 +1,13 @@
-import { precacheAndRoute } from 'workbox-precaching';
+import {precacheAndRoute, cleanupOutdatedCaches} from 'workbox-precaching';
 import {setCacheNameDetails} from 'workbox-core';
 
 setCacheNameDetails({prefix: "app.elcoop.io"});
+cleanupOutdatedCaches()
+
 precacheAndRoute(self.__WB_MANIFEST);
 
 self.addEventListener('message', (message) => {
-    console.log('message',message);
+    console.log('message', message);
     if (message.data.action === 'skipWaiting') {
         self.skipWaiting();
     }
