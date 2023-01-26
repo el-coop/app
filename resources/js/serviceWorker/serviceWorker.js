fa8f8c0d69e4bdb029070aed62e7c8fcd908bbcb
Nur Bar <LCD344@yahoo.com>
2023-01-25 23:15:31 -0300

    added caches

--- resources/js/serviceWorker/serviceWorker.js
+++ resources/js/serviceWorker/serviceWorker.js
@@ -1,10 +1,29 @@
 import {precacheAndRoute, cleanupOutdatedCaches} from 'workbox-precaching';
 import {setCacheNameDetails} from 'workbox-core';
+import { CacheFirst  } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+
 
 setCacheNameDetails({prefix: "app.elcoop.io"});
 cleanupOutdatedCaches()
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+    new RegExp('/images/.*'),
+    new CacheFirst({
+        cacheName: 'image-cache',
+    })
+);
+
+
+registerRoute(
+    new RegExp('/assets/.*\.otf'),
+    new CacheFirst({
+        cacheName: 'font-cache',
+    })
+);
+
 
 self.addEventListener('message', (message) => {
     console.log('message', message);
